73ea29cc77d36a6d859a3783ec506de68e01b1ca
Nezemnoy <vdn@tut.by>
2018-10-30 12:17:29 +0300

    4th commit

---
+++ validate.js
@@ -0,0 +1,10 @@
+// function validate(javascript_form, email) {
+//     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+//     var address = document.forms[javascript_form].elements[email].value;
+//     if(reg.test(address) == false) {
+//        alert('Неверный формат адреса электронной почты!');
+//        document.forms[javascript_form].elements[email].value = "";
+//        return false;
+//     }
+//  }
+
